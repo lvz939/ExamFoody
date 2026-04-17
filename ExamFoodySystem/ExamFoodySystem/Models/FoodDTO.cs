e63003d8ef9c8e1e5aa0484086722bbe3a59f2e5
lvz939 <kfl_123@abv.bg>
2026-04-17 11:53:28 +0200

    Added Nunit project

---
+++ ExamFoodySystem/ExamFoodySystem/Models/FoodDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace ExamFoodySystem.Models
+{
+    internal class FoodDTO
+    {
+
+        [JsonPropertyName("name")]
+
+        public string? Name { get; set; }
+
+        [JsonPropertyName("description")]
+
+        public string? Description { get; set; }
+
+        [JsonPropertyName("url")]
+
+        public string? Url { get; set; }
+    }
+}
